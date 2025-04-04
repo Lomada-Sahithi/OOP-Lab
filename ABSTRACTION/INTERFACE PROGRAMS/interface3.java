d2e9a6be2f293b92c0c1009d13868959a565f5bd
Lomada-Sahithi <lomadasahithi@gmail.com>
2025-04-04 09:38:23 +0530

    Create interface3.java

---
+++ ABSTRACTION/INTERFACE PROGRAMS/interface3.java
@@ -0,0 +1,43 @@
+interface Shape3D { 
+double volume(); 
+double surfaceArea(); }  
+class Sphere implements Shape3D {
+ double radius;  
+
+Sphere(double radius) {  
+    this.radius = radius;  
+}  
+ 
+public double volume() {  
+    return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);  
+}  
+ 
+public double surfaceArea() {  
+    return 4 * Math.PI * Math.pow(radius, 2);  
+}   
+}  
+class Cube implements Shape3D { 
+double side;  
+
+Cube(double side) {  
+    this.side = side;  
+}   
+public double volume() {  
+    return Math.pow(side, 3);  
+}    
+public double surfaceArea() {  
+    return 6 * Math.pow(side, 2);  
+}   
+  
+
+}  
+public class Record{ 
+public static void main(String[] args) {
+ Shape3D sphere = new Sphere(7.0); Shape3D cube = new Cube(6.0);  
+
+   System.out.println("Sphere Volume: " + sphere.volume());  
+    System.out.println("Sphere Surface Area: " + sphere.surfaceArea());  
+    System.out.println("Cube Volume: " + cube.volume());  
+    System.out.println("Cube Surface Area: " + cube.surfaceArea());  
+}   
+}
